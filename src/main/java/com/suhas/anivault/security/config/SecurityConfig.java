353b0c9e527b1eabbd3c7bb414a84f47a48376e8
Suhasud <suhasdinesh68@gmail.com>
2026-08-12 02:27:44 +0530

    Allow Railway origin in CORS

--- src/main/java/com/suhas/anivault/security/config/SecurityConfig.java
+++ src/main/java/com/suhas/anivault/security/config/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:3000")
+                List.of("http://localhost:3000",
+                        "https://anivault-production-74a7.up.railway.app")
         );
 
         configuration.setAllowedMethods(
